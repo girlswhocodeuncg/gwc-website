c0d378c70bb4a2d02444e8278f4b7aa04108f28a
“Jada <grahamjada14@gmail.com>
2025-11-29 13:03:11 -0500

    added necassary elements to home/index page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 console.log("GWC Homepage Loaded");
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("shadow-sm", window.scrollY >10);
+})
