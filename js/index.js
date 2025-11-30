3b822d75a1476e25ee1d3119d6bdf50acbac0874
“Jada <grahamjada14@gmail.com>
2025-11-30 14:49:10 -0500

    updated font and attempted to fix position of pictures and text.

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ console.log("GWC Homepage Loaded");
 window.addEventListener("scroll", () => {
     const nav = document.querySelector("nav");
     nav.classList.toggle("shadow-sm", window.scrollY >10);
-})
+});
